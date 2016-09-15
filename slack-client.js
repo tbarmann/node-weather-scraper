be908ae2996b1e532ce5f9298aea80df7bcd2ca5
Timothy Barmann <tbarmann92@gmail.com>
2016-09-15 18:08:57 -0400

    Slack client listening to messages

--- slack-client.js
+++ slack-client.js
@@ -2,32 +2,29 @@ var RtmClient = require('@slack/client').RtmClient;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 var MemoryDataStore = require('@slack/client').MemoryDataStore;
+
 var config = require('./.config.json');
 var token = config.SLACK_API_TOKEN;
 
-var rtm = new RtmClient(token, {node
-  logLevel: 'error', // check this out for more on logger: https://github.com/winstonjs/winston
-  dataStore: new MemoryDataStore() // pass a new MemoryDataStore instance to cache information
-});
 
+//var token = process.env.SLACK_API_TOKEN || '';
+
+var rtm = new RtmClient(token, {
+  logLevel: 'error',
+  dataStore: new MemoryDataStore()
+});
 rtm.start();
 
-rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function handleRTMAuthenticated() {
-  console.log('RTM client authenticated!');
+
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
+  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
 });
 
-rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
+rtm.on(RTM_EVENTS.MESSAGE, function (message) {
   console.log(
-    'User %s posted a message in %s channel',
+    'User "%s" posted a message in channel: "%s"',
     rtm.dataStore.getUserById(message.user).name,
     rtm.dataStore.getChannelGroupOrDMById(message.channel).name
   );
 });
 
-rtm.on(RTM_EVENTS.REACTION_ADDED, function handleRtmReactionAdded(reaction) {
-  console.log('Reaction added:', reaction);
-});
-
-rtm.on(RTM_EVENTS.REACTION_REMOVED, function handleRtmReactionRemoved(reaction) {
-  console.log('Reaction removed:', reaction);
-});
