5f42c98a04706051c997865f751a89bd8e0ef40d
Timothy Barmann <tbarmann92@gmail.com>
2016-09-16 19:16:16 -0400

    Check timezone

--- slack-client.js
+++ slack-client.js
@@ -20,6 +20,8 @@ rtm.start();
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
 	weatherBotId = rtmStartData.self.id; // U1VJTP4TS
   weatherBotName = rtmStartData.self.name;
+  const now = new Date();
+  console.log(now);
 	console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
 });
 
