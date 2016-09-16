b715b8994fe5c364526327cb44f82cdec61b43af
Timothy Barmann <tbarmann92@gmail.com>
2016-09-16 10:47:36 -0400

    MVP working version

--- slack-client.js
+++ slack-client.js
@@ -1,50 +1,47 @@
-var _ = require('lodash');
-var RtmClient = require('@slack/client').RtmClient;
-var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
-var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
-var RTM_CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS.RTM;
-
-var MemoryDataStore = require('@slack/client').MemoryDataStore;
-
-var config = require('./.config.json');
-var token = config.SLACK_API_TOKEN;
-var weatherBotId;
+"use strict";
+const _ = require('lodash');
+const RtmClient = require('@slack/client').RtmClient;
+const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+const RTM_CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS.RTM;
+const MemoryDataStore = require('@slack/client').MemoryDataStore;
+const config = require('./.config.json');
+const token = config.SLACK_API_TOKEN;
 const getWeatherData = require('./nws-current-conditions-scraper');
+let weatherBotId;
 
-
-//var token = process.env.SLACK_API_TOKEN || '';
-
-var rtm = new RtmClient(token, {
+const rtm = new RtmClient(token, {
   logLevel: 'error',
   dataStore: new MemoryDataStore()
 });
+
 rtm.start();
 
-
-rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
 	weatherBotId = rtmStartData.self.id;
 	console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
 });
 
-rtm.on(RTM_EVENTS.MESSAGE, function (message) {
-  if (message.subtype !== 'message_deleted' && isMessageToUserId(message.text,weatherBotId)) {
+rtm.on(RTM_EVENTS.MESSAGE, (message) => {
+  if (message.subtype !== 'message_deleted' && messageContainsUserId(message.text,weatherBotId)) {
   	getWeatherReport(message.channel);
   	console.log('The message mentions me!');
   }
 });
 
-function getWeatherReport(channelId) {
+const getWeatherReport = (channelId) => {
 	const url = 'http://w1.weather.gov/data/obhistory/KPVD.html';
 	getWeatherData(url, (data) => {
-		console.log(data[0].temp_air);
-		rtm.sendMessage(data[0].temp_air, channelId, () => {
+    const currentTemp = data.data[0].temp_air;
+		console.log(currentTemp);
+		rtm.sendMessage("The current temperature is " + currentTemp, channelId, () => {
 			console.log('message sent.');
- 
+
 		});
 	})
 }
 
-function isMessageToUserId(msg, userId) {
-	pattern  = '<@' + userId + '>';
+const messageContainsUserId = (msg, userId) => {
+	const pattern  = '<@' + userId + '>';
 	return (msg.indexOf(pattern) !== -1);
 }
