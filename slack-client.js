48717bb2329b482027c479ba79085cef1137c15d
Timothy Barmann <tbarmann92@gmail.com>
2016-09-15 22:10:21 -0400

    WIP fetching weather and responding

--- slack-client.js
+++ slack-client.js
@@ -1,10 +1,15 @@
+var _ = require('lodash');
 var RtmClient = require('@slack/client').RtmClient;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+var RTM_CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS.RTM;
+
 var MemoryDataStore = require('@slack/client').MemoryDataStore;
 
 var config = require('./.config.json');
 var token = config.SLACK_API_TOKEN;
+var weatherBotId;
+const getWeatherData = require('./nws-current-conditions-scraper');
 
 
 //var token = process.env.SLACK_API_TOKEN || '';
@@ -17,14 +22,29 @@ rtm.start();
 
 
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
-  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+	weatherBotId = rtmStartData.self.id;
+	console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
 });
 
 rtm.on(RTM_EVENTS.MESSAGE, function (message) {
-  console.log(
-    'User "%s" posted a message in channel: "%s"',
-    rtm.dataStore.getUserById(message.user).name,
-    rtm.dataStore.getChannelGroupOrDMById(message.channel).name
-  );
+  if (message.subtype !== 'message_deleted' && isMessageToUserId(message.text,weatherBotId)) {
+  	sendWeatherReport(message.channel);
+  	console.log('The message mentions me!');
+  }
 });
 
+function sendWeatherReport(channelId) {
+	const url = 'http://w1.weather.gov/data/obhistory/KPVD.html';
+	getWeatherData(url, function(data){
+  	var msg = data[0];
+  	console.log(msg);
+  	rtm.sendMessage(msg, channelId, function messageSent() {
+    	console.log('message sent.');
+  	});
+	});
+}
+
+function isMessageToUserId(msg, userId) {
+	pattern  = '<@' + userId + '>';
+	return (msg.indexOf(pattern) !== -1);
+}
