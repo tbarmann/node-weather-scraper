eab56aa494fea6e2f77bd4bda9703c9c831833e4
Timothy Barmann <tbarmann92@gmail.com>
2016-10-02 22:41:18 -0400

    Add timezone and more to airports.json

--- has-weather.js
+++ has-weather.js
@@ -2,15 +2,23 @@
 const fs = require('fs');
 const request = require('request');
 const airports = require('./airports.json');
-const states_hash = require('./states.json');
+const stations = require('./stations.json');
 const _ = require('lodash');
 
 const newAirports = [];
 
-
 _.each(airports, (thisStation) => {
-  thisStation.state_abbr = states_hash[thisStation.state];
-  newAirports.push(thisStation);
+  const stationRecord = _.find(stations, (o) => o.STID === thisStation.stationId);
+  if (stationRecord) {
+    thisStation.timeZone = stationRecord.TimeZone;
+    thisStation.dayTimeSaving = stationRecord.DayTimeSaving;
+    thisStation.latitude = stationRecord.Latitude;
+    thisStation.longitude = stationRecord.Longitude;
+  }
+  else {
+    console.log(`Not found: ${thisStation.stationId}`);
+  }
+    newAirports.push(thisStation);
 });
 
 fs.writeFile('./new-airports.json', JSON.stringify(newAirports, null, '  '));
