2ca4e8d71cfd687ab77f26ee9d5e5020ae7ec054
Timothy Barmann <tbarmann92@gmail.com>
2016-10-22 16:13:08 -0400

    Add mocha and chai for testing

---
+++ test/sample-test.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect
+
+describe('A basic test', function(){
+	it('should pass when everything is set up correctly', function(){
+    expect(true).to.be.true;
+  });
+});
