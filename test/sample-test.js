a6ddcc725590b26838f2d46326b7acca5a21e6a6
Timothy Barmann <tbarmann92@gmail.com>
2016-10-25 09:20:24 -0400

    Fix cache test

--- test/sample-test.js
+++ test/sample-test.js
@@ -18,26 +18,86 @@ const stationRecords = [
     "dayTimeSaving": "y",
     "latitude": 41.73,
     "longitude": -71.43
+  },
+  {
+    "stationId": "KDEN",
+    "airportId": "DEN",
+    "name": "Denver International Airport",
+    "city": "Denver",
+    "state": "Colorado",
+    "alternate_city": "Boulder",
+    "state_abbr": "CO",
+    "timeZone": "M",
+    "dayTimeSaving": "y",
+    "latitude": 39.87,
+    "longitude": -104.67
+  },
+  {
+    "stationId": "KLAX",
+    "airportId": "LAX",
+    "name": "Los Angeles International Airport",
+    "city": "Los Angeles",
+    "state": "California",
+    "alternate_city": "",
+    "state_abbr": "CA",
+    "timeZone": "P",
+    "dayTimeSaving": "y",
+    "latitude": 33.94,
+    "longitude": -118.4
+  },
+  {
+    "stationId": "KORD",
+    "airportId": "ORD",
+    "name": "O'Hare International Airport",
+    "city": "Chicago",
+    "state": "Illinois",
+    "alternate_city": "",
+    "state_abbr": "IL",
+    "timeZone": "C",
+    "dayTimeSaving": "y",
+    "latitude": 41.98,
+    "longitude": -87.9
+  },
+  {
+    "stationId": "KIWA",
+    "airportId": "IWA",
+    "name": "Phoenix-Mesa Gateway Airport",
+    "city": "Mesa",
+    "state": "Arizona",
+    "alternate_city": "",
+    "state_abbr": "AZ",
+    "timeZone": "M",
+    "dayTimeSaving": "n",
+    "latitude": 33.3,
+    "longitude": -111.67
   }
 ];
 
-describe('A basic test', function(){
-	it('should pass when everything is set up correctly', function(){
+const spy = sinon.spy();
+const emitter = new EventEmitter;
+const myCache = new WeatherReportCache(emitter);
+
+describe('A basic test', () => {
+  it('should pass when everything is set up correctly', () => {
     expect(true).to.be.true;
   });
 });
 
-describe('EventEmitter', function(){
-  describe('#emit()', function(){
-    it('should invoke the callback', function(){
-      const emitter = new EventEmitter;
-      const myCache = new WeatherReportCache(emitter);
-      const spy = sinon.spy();
+describe('EventEmitter', () => {
+  beforeEach( (done) => {
+    spy.reset();
+    emitter.on('fetch_done', () => {
+      spy();
+      done();
+    });
+    myCache.getWeather(stationRecords[0]);
+  })
 
-      emitter.on('fetch_done', spy);
-      myCache.getWeather(stationRecords[0]);
-  //    spy.called.should.equal.true;
-      sinon.assert.calledOnce(spy);
-    })
+  // it('should invoke the callback', () => {
+  //   sinon.assert.calledOnce(spy);
+  // })
+
+  it('should have 1 item in cache', () => {
+    expect(Object.keys(myCache.cache).length).to.equal(1);
   })
 })
